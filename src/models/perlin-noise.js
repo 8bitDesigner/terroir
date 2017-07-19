9139f8ae00a12f173f606dbc95d766a741ea9222
Paul Sweeney <paul@8-bitdesign.com>
2017-07-19 16:52:23 -0700

    Adding multipass perlin noise generator

--- src/models/perlin-noise.js
+++ src/models/perlin-noise.js
@@ -1,25 +1,42 @@
 /* eslint-disable no-multi-spaces */
 import noise from 'noisejs-umd'
 import AbstractGenerator from './base'
+import { floor } from '../lib/rand-utils'
 
 export default class PerlinNoiseGenerator extends AbstractGenerator {
   constructor (exponent, type = 'perlin2') {
     super(exponent)
+    this.erosion = -0.85
     this.type = type
     this.reset()
   }
 
   reset () {
-    noise.seed(Math.random())
     super.reset()
+    this.noise(100, 256, this.size - 1)
   }
 
-  run () {
-    for (var x = 0; x < this.size; x++) {
-      for (var y = 0; y < this.size; y++) {
-        var value = noise.perlin2(x / 50, y / 50)
-        this.set(x, y, Math.floor(Math.abs(value) * 256))
+  apply (scale, range) {
+    for (let x = 0; x < this.size; x++) {
+      for (let y = 0; y < this.size; y++) {
+        const value = noise.perlin2(x / scale, y / scale)
+        const current = this.get(x, y) || 0
+        this.set(x, y, current + Math.floor(Math.abs(value) * range))
       }
+    }
+  }
+
+  noise (scale, range, steps) {
+    noise.seed(Math.random())
+    this.apply(scale, range)
+    this.normalize()
+
+    if (steps > 2) {
+      scale = scale / 2
+      range = floor(range * Math.pow(2.0, this.erosion))
+      steps = steps - 1
+
+      this.queue.push(() => this.noise(scale, range, steps))
     }
   }
 }
